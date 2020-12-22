d5e5ba1942686580c7558ee51598d24cb021d617
liuwentao <>
2020-12-22 20:33:45 +0800

    底部添加tabs

---
+++ pages/tabbar/tabbar-1/tabbar-1.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		页面 - 1
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style>
+.content {
+	text-align: center;
+	height: 400upx;
+	margin-top: 200upx;
+}
+</style>
